92823a81450f7faf1bceb8b472915535868826c0
Guillermo Antonio Palomino Sosa <guillermo.a.palomino.sosa@intel.com>
2025-01-24 18:51:24 -0600

    Initial documentation

---
+++ docs/index.md
@@ -0,0 +1,16 @@
+# EDK2Code
+
+<a class="github-button" href="https://github.com/intel/Edk2Code" data-size="large" data-show-count="true" aria-label="Star intel/Edk2Code on GitHub">Star</a>
+
+The EDK2Code Extension for Visual Studio Code enhances the development experience for engineers working with the EDK2 (UEFI) codebase by providing features such as syntax highlighting, code navigation, and error detection. This extension integrates these capabilities directly into Visual Studio Code, enabling developers to streamline their workflow and increase productivity.
+
+By leveraging the EDK2Code extension, developers can easily navigate through code, quickly identify errors, and benefit from improved readability of EDK2 source files. This results in a more efficient development process and a smoother coding experience.
+
+Additionally, the extension supports various EDK2-specific commands accessible via the command palette, such as rebuilding the index database and configuring the workspace. This comprehensive toolset empowers developers to work more effectively with EDK2 projects, ultimately leading to higher quality code and faster development cycles.
+
+Check YouTube video presentation on **UEFIForum** Channel
+
+[![YouTube presentation](https://img.youtube.com/vi/2Js9nSPKtd8/0.jpg)](https://www.youtube.com/watch?v=2Js9nSPKtd8)
+
+<a class="btn btn-info" href="https://github.com/intel/Edk2Code"><span class="codicon codicon-github-inverted" title="Copy to clipboard"></span> Github</a>
+<a class="btn btn-info" href="https://marketplace.visualstudio.com/items?itemName=intel-corporation.edk2code"><span class="codicon codicon-beaker" title="Copy to clipboard"></span>VSCODE</span></a>
---
+++ docs/theme/js/extra.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+    function checkHidePanel() {
+      const urlParams = new URLSearchParams(window.location.search);
+      if (urlParams.get('hidepanel') === 'TRUE') {
+        document.querySelector('.wy-nav-side').style.display = 'none';
+        document.querySelector('.rst-content').style.display = 'block';
+        document.querySelectorAll('.rst-content a').forEach(function(link) {
+          link.setAttribute('target', '_blank');
+        });
+      }
+    }
+
+    checkHidePanel();    
+
+    // Section copy button
+    var headers = document.querySelectorAll('h1, h2, h3, h4');
+    headers.forEach(function(header) {
+        header.classList.add('header-link'); // Add this line to add a class
+        var copyButton = '<span class="btn-clipboard-link codicon codicon-link" title="Copy to clipboard"></span>';
+        console.log("copied");
+        header.insertAdjacentHTML('beforeend', copyButton);
+      });
+
+    var clipboardLink = new ClipboardJS('.btn-clipboard-link', {
+      text: function (trigger) {
+        var url = window.location.href.split("#")[0] + '#' + trigger.parentElement.id;
+        return url;
+      }
+    });
+
+    clipboardLink.on('success', function (e) {
+      e.clearSelection();
+  
+      // https://atomiks.github.io/tippyjs/v6/all-props/
+      var tippyInstance = tippy(
+        e.trigger,
+        {
+          content: 'Copied',
+          showOnCreate: true,
+          trigger: 'manual',
+        },
+      );
+      setTimeout(function() { tippyInstance.hide(); }, 1000);
+    });
+
+    // https://clipboardjs.com/
+    var selectors = document.querySelectorAll('pre code');
+    var copyButton = '<div class="clipboard"><span class="btn-clipboard codicon codicon-copy" title="Copy to clipboard"></div>';
+    Array.prototype.forEach.call(selectors, function(selector){
+      selector.insertAdjacentHTML('beforebegin', copyButton);
+    });
+    var clipboard = new ClipboardJS('.btn-clipboard', {
+      target: function (trigger) {
+        return trigger.parentNode.nextElementSibling;
+      }
+    });
+  
+    clipboard.on('success', function (e) {
+      e.clearSelection();
+  
+      // https://atomiks.github.io/tippyjs/v6/all-props/
+      var tippyInstance = tippy(
+        e.trigger,
+        {
+          content: 'Copied',
+          showOnCreate: true,
+          trigger: 'manual',
+        },
+      );
+      setTimeout(function() { tippyInstance.hide(); }, 1000);
+    });
+  });
