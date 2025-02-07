a5b5168ca05e0030fda7836ed917c5dbd6dd1b13
Guillermo Antonio Palomino Sosa <guillermo.a.palomino.sosa@intel.com>
2025-02-07 16:48:43 -0600

    Bump version to 1.1.0 and add release notes for new features

--- src/newVersionPage/newVersionMessage.ts
+++ src/newVersionPage/newVersionMessage.ts
@@ -1,30 +1,18 @@
 import { getCurrentVersion } from "../utils";
 import * as vscode from 'vscode';
-
+import * as path from 'path';
+import * as fs from 'fs';
 
 export async function showReleaseNotes(context: vscode.ExtensionContext){
     const CURRENT_VERSION = getCurrentVersion();
     const previousVersion = context.globalState.get<string>('extensionVersion');
     if (previousVersion !== CURRENT_VERSION) {
-        const panel = vscode.window.createWebviewPanel(
-            'releaseNotes',
-            `EDK2Code Release Notes ${CURRENT_VERSION}`,
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true,
-            }
-        );
-
-        panel.webview.html = `
-<h1>Edk2Code <strong>${CURRENT_VERSION}</strong></h1>
-<p>Thanks for using the <strong>Edk2Code Vscode extension</strong>.</p>
-<p>🎉 If you find this extension useful 🎉:</p>
-<ul>
-<li>⭐ Give a start on <a href="https://github.com/intel/Edk2Code">Github</a></li>
-<li>📜 Leave a review in <a href="https://marketplace.visualstudio.com/items?itemName=intel-corporation.edk2code">Vscode marketplace</a></li>
-</ul>
-<h2><a href="https://intel.github.io/Edk2Code/releases/${CURRENT_VERSION}/">View new features</a></h2>
-           `;
         await context.globalState.update('extensionVersion', CURRENT_VERSION);
+        
+        const releaseNotesPath = path.join(context.extensionPath, 'src', 'newVersionPage', `${CURRENT_VERSION}.md`);
+        if (fs.existsSync(releaseNotesPath)) {
+            const releaseNotesUri = vscode.Uri.file(releaseNotesPath);
+            await vscode.commands.executeCommand('markdown.showPreview', releaseNotesUri);
+        }
     }
 }
